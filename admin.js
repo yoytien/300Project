fc04defd8b7093dfc8e8311a1cf519d66f65d1cb
yoytien <yewtian.chin@hotmail.com>
2018-11-28 11:53:11 +0800

    first web

---
+++ admin.js
@@ -0,0 +1,11 @@
+window.alert("in the admin.js");
+
+var submit = document.getElementById("AButon");
+
+
+function submit1(){
+    window.alert("in the onclick function");
+var firebaseRef = firebase.database().ref();
+firebaseRef.child("Matric").set("135423")
+window.alert("update the firebase");
+}
---
+++ login.js
@@ -0,0 +1,16 @@
+var app_fireBase = {};
+(function(){
+   
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCfAwKSZ-q3GaiPK8Di5bWCCh8jTImbdI",
+    authDomain: "sas92-10d48.firebaseapp.com",
+    databaseURL: "https://sas92-10d48.firebaseio.com",
+    projectId: "sas92-10d48",
+    storageBucket: "sas92-10d48.appspot.com",
+    messagingSenderId: "501650310659"
+  };
+  firebase.initializeApp(config);
+  app_fireBase =firebase;
+
+})()
