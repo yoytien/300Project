119f398fd944be94a0971887d30194a8f4b03d0c
yoytien <yewtian.chin@hotmail.com>
2018-11-29 23:43:29 +0800

    upadate firebase.js with yewtian conf

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,18 @@ import * as firebase from 'firebase';
 
 
 const config = {
-    apiKey: "AIzaSyBRjhvoFAKPcjElLwoU6Qc5yDNQMvRFtXk",
-    authDomain: "sas1database-cdade.firebaseapp.com",
-    databaseURL: "https://sas1database-cdade.firebaseio.com",
-    projectId: "sas1database-cdade",
-    storageBucket: "sas1database-cdade.appspot.com",
-    messagingSenderId: "1022766601181"
+    // apiKey: "AIzaSyBRjhvoFAKPcjElLwoU6Qc5yDNQMvRFtXk",
+    // authDomain: "sas1database-cdade.firebaseapp.com",
+    // databaseURL: "https://sas1database-cdade.firebaseio.com",
+    // projectId: "sas1database-cdade",
+    // storageBucket: "sas1database-cdade.appspot.com",
+    // messagingSenderId: "1022766601181"
+    apiKey: "AIzaSyDCfAwKSZ-q3GaiPK8Di5bWCCh8jTImbdI",
+    authDomain: "sas92-10d48.firebaseapp.com",
+    databaseURL: "https://sas92-10d48.firebaseio.com",
+    projectId: "sas92-10d48",
+    storageBucket: "sas92-10d48.appspot.com",
+    messagingSenderId: "501650310659"
   };
 
   firebase.initializeApp(config);
